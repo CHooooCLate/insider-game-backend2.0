67ccc9008e08ea39af79a3f8bfc4928d9f3f6d4e
CHooooCLate <kintaro_9161114_1@yahoo.co.jp>
2020-12-14 14:22:02 +0900

    [fix] disconnectでレコード削除できていない不具合を修正

--- ondisconnect/lambda_function.py
+++ ondisconnect/lambda_function.py
@@ -1,11 +1,24 @@
 import boto3
 
+# デバッグするときに使う
+from pprint import pprint
+
+from boto3.dynamodb.conditions import Key, Attr
+
 dynamodb = boto3.resource('dynamodb')
 playerTable = dynamodb.Table('Player')
 
 def lambda_handler(event, context):
     connection_id = event.get('requestContext',{}).get('connectionId')
+    player = playerTable.scan(
+         FilterExpression=Attr('id').eq(connection_id)
+    ).get('Items')
 
-    playerTable.delete_item(Key={ 'id': connection_id })
+    playerTable.delete_item(
+        Key = {
+            'id': connection_id,
+            'game_id': player[0]['game_id']
+        }
+    )
 
     return { 'statusCode': 200, 'body': 'ok' }
